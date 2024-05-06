6699ecf19f3dfa3418f1c6974ab2191783cd0045
manyu-optimizer <abhimanyu.gupta@wheelsup.com>
2024-05-06 13:14:31 +0530

    updated deletion logic

--- repository/user_storage.py
+++ repository/user_storage.py
@@ -15,7 +15,12 @@ class User:
             self.user_storage.data_store = dict() # key will be id and value will be board
 
     def put_user(self, user_id, user):
-        if not hasattr(self,'user_storage'):
+        if hasattr(self,'user_storage'):
             self.user_storage.data_store[user_id] = user
             print(f"user added successfully")
+
+    def delete_user(self, user_id):
+        if hasattr(self, 'user_storage'):
+            del self.user_storage.data_store[user_id]
+            print(f"user id : {user_id} is successfully deleted from user storage")
 
--- service/user_managing_service.py
+++ service/user_managing_service.py
@@ -1,22 +1,33 @@
 from abc import ABC,abstractmethod
 from PYTHON_TRELLO_LLD.model.User import *
+from PYTHON_TRELLO_LLD.Utilities.helpers import generate_random_number
+from PYTHON_TRELLO_LLD.Utilities.enums import ModelEnum
+from PYTHON_TRELLO_LLD.repository.user_storage import User
 
 class Userservice(ABC):
     @abstractmethod
-    def create(self,user_id,name,email):
+    def create(self,name,email):
+        pass
+
+    @abstractmethod
+    def delete_user(self,user_id):
         pass
 
 
 
 class UserserviceImpl(Userservice):
     
-    def create(self,user_id,name,email):
+    def create(self,name,email):
+        generated_user_id = generate_random_number(ModelEnum.USER)
         
         user_builder = UserBuilder();
-        user = user_builder.set_user_id(user_id).set_email(email).set_name(name).build()
-        
+        user = user_builder.set_user_id(generated_user_id).set_email(email).set_name(name).build()
+        user_repository = User()
+        user_repository.put_user(generated_user_id, user)
         return user
-        
-    
+
+    def delete_user(self,user_id):
+        user_repository = User()
+        user_repository.delete_user(user_id)
     
     
